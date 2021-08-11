df217d0abad85c7da91f4925aced39fa126ba1c9
bassel-mousa <--get>
2021-08-12 00:40:40 +0300

    First Initializing

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/views/NewsActivity.kt
@@ -0,0 +1,14 @@
+package com.androiddevs.mvvmnewsapp.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.androiddevs.mvvmnewsapp.R
+
+class NewsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+
+    }
+}
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/views/fragments/ArticleFragment.kt
@@ -0,0 +1,7 @@
+package com.androiddevs.mvvmnewsapp.views.fragments
+
+import androidx.fragment.app.Fragment
+import com.androiddevs.mvvmnewsapp.R
+
+class ArticleFragment : Fragment(R.layout.fragment_article) {
+}

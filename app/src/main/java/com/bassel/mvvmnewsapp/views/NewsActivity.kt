7795bcac6c10731c9118522f575cc235f4e76e52
bassel-mousa <--get>
2021-08-13 21:42:27 +0300

    Conect News Repository With Fragment

--- app/src/main/java/com/bassel/mvvmnewsapp/views/NewsActivity.kt
+++ app/src/main/java/com/bassel/mvvmnewsapp/views/NewsActivity.kt
@@ -2,16 +2,28 @@ package com.bassel.mvvmnewsapp.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.bassel.mvvmnewsapp.R
+import com.bassel.mvvmnewsapp.database.ArticleDatabase
+import com.bassel.mvvmnewsapp.repository.NewsRepository
+import com.bassel.mvvmnewsapp.util.providers.NewsViewModelProviderFactory
+import com.bassel.mvvmnewsapp.viewmodel.NewsViewModel
 import kotlinx.android.synthetic.main.activity_news.*
 
 class NewsActivity : AppCompatActivity() {
 
+    lateinit var viewModel: NewsViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
+        viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
+
         bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController());
     }
 }

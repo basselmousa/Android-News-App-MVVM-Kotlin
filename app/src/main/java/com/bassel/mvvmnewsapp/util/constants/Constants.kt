acaa6fc724b69fcbcda2f6169fe0d9bac6e861b5
bassel-mousa <--get>
2021-08-13 21:43:01 +0300

    Adding Delay Time Constant

--- app/src/main/java/com/bassel/mvvmnewsapp/util/constants/Constants.kt
+++ app/src/main/java/com/bassel/mvvmnewsapp/util/constants/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object {
         const val API_KEY = "71d9a51f0aa34899ac9e89466d54eaa2"
         const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }

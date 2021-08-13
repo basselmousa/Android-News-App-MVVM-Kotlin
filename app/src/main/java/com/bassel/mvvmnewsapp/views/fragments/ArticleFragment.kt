04e029c26ad47847a1d6f49b5cf1a9df1d3bc18a
bassel-mousa <--get>
2021-08-13 21:41:21 +0300

    Showing Articel In Web View Inside The Application

--- app/src/main/java/com/bassel/mvvmnewsapp/views/fragments/ArticleFragment.kt
+++ app/src/main/java/com/bassel/mvvmnewsapp/views/fragments/ArticleFragment.kt
@@ -1,7 +1,35 @@
 package com.bassel.mvvmnewsapp.views.fragments
 
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.bassel.mvvmnewsapp.R
+import com.bassel.mvvmnewsapp.viewmodel.NewsViewModel
+import com.bassel.mvvmnewsapp.views.NewsActivity
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_article.*
 
 class ArticleFragment : Fragment(R.layout.fragment_article) {
+    private lateinit var viewModel: NewsViewModel
+
+    val args : ArticleFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        this.viewModel = (activity as NewsActivity).viewModel
+
+        val article = args.article
+
+        webView.apply {
+            webViewClient = WebViewClient()
+            loadUrl(article.url)
+        }
+
+        fab.setOnClickListener {
+            viewModel.saveArticle(article)
+            Snackbar.make(view, "Article Saved Successfully", Snackbar.LENGTH_SHORT).show()
+        }
+    }
 }

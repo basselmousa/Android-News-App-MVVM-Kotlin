6da17996c2ce560a0740667a9fbeec7b72cfb4d8
bassel-mousa <--get>
2021-08-14 00:13:42 +0300

    MAking Query Size Constants

--- app/src/main/java/com/bassel/mvvmnewsapp/util/constants/Constants.kt
+++ app/src/main/java/com/bassel/mvvmnewsapp/util/constants/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
         const val API_KEY = "71d9a51f0aa34899ac9e89466d54eaa2"
         const val BASE_URL = "https://newsapi.org/"
         const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }

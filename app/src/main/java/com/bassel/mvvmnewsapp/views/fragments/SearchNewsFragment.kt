ad5045c31a60a965d9e94f0c73639d875d6a5005
bassel-mousa <--get>
2021-08-13 21:38:56 +0300

    Implement Functionality For Searching For News

--- app/src/main/java/com/bassel/mvvmnewsapp/views/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/bassel/mvvmnewsapp/views/fragments/SearchNewsFragment.kt
@@ -1,7 +1,102 @@
 package com.bassel.mvvmnewsapp.views.fragments
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.core.widget.addTextChangedListener
+
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bassel.mvvmnewsapp.R
+import com.bassel.mvvmnewsapp.adapters.NewsAdapter
+import com.bassel.mvvmnewsapp.util.constants.Constants
+//import com.bassel.mvvmnewsapp.util.constants.Constants.Companion.SEARCH_NEWS_TIME_DELAY
+import com.bassel.mvvmnewsapp.util.network.Resource
+import com.bassel.mvvmnewsapp.viewmodel.NewsViewModel
+import com.bassel.mvvmnewsapp.views.NewsActivity
+import kotlinx.android.synthetic.main.fragment_search_news.*
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
 
 class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
+
+    private lateinit var viewModel: NewsViewModel
+
+    lateinit var newsAdapter: NewsAdapter
+
+    private val TAG = "SearchNewsFragment"
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        this.viewModel = (activity as NewsActivity).viewModel
+        setupRecyclerView()
+
+        newsAdapter.setOnItemClickListener {
+            var bundle = Bundle().apply {
+                putSerializable("article", it)
+            }
+
+            findNavController().navigate(
+                R.id.action_searchNewsFragment_to_articleFragment,
+                bundle
+            )
+        }
+
+
+        var job: Job? = null
+        etSearch.addTextChangedListener { editable ->
+            job?.cancel()
+            job = MainScope().launch {
+                delay(Constants.SEARCH_NEWS_TIME_DELAY)
+                editable?.let {
+                    if (editable.toString().isNotEmpty()){
+                        viewModel.searchNews(editable.toString())
+                    }
+                }
+            }
+        }
+
+        viewModel.searchNews.observe(viewLifecycleOwner, Observer { response ->
+            when (response) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let { newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Log.e(TAG, "An error occurred: $message")
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+
+                }
+            }
+        })
+    }
+
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapter()
+        rvSearchNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+
+    private fun hideProgressBar() {
+        paginationProgressBar.visibility = View.INVISIBLE
+    }
+
+    private fun showProgressBar() {
+        paginationProgressBar.visibility = View.VISIBLE
+    }
 }

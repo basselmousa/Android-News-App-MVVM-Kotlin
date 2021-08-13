538b74b214e3247d554946f9eae1907b504076f4
bassel-mousa <--get>
2021-08-13 21:40:25 +0300

    Implement Functionality To Show News By Country Code

--- app/src/main/java/com/bassel/mvvmnewsapp/views/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/bassel/mvvmnewsapp/views/fragments/BreakingNewsFragment.kt
@@ -1,7 +1,80 @@
 package com.bassel.mvvmnewsapp.views.fragments
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bassel.mvvmnewsapp.R
+import com.bassel.mvvmnewsapp.adapters.NewsAdapter
+import com.bassel.mvvmnewsapp.util.network.Resource
+import com.bassel.mvvmnewsapp.viewmodel.NewsViewModel
+import com.bassel.mvvmnewsapp.views.NewsActivity
+import kotlinx.android.synthetic.main.fragment_breaking_news.*
 
 class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
+
+    private lateinit var viewModel: NewsViewModel
+
+    lateinit var newsAdapter: NewsAdapter
+
+    private val TAG = "BreakingNewsFragment"
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        this.viewModel = (activity as NewsActivity).viewModel
+
+        setupRecyclerView()
+
+        newsAdapter.setOnItemClickListener() {
+            println("Clicked")
+            val bundle = Bundle().apply {
+                putSerializable("article", it)
+            }
+
+            findNavController().navigate(
+                R.id.action_breakingNewsFragment_to_articleFragment,
+                bundle
+            )
+        }
+
+        viewModel.breakingNews.observe(viewLifecycleOwner, Observer { response->
+            when(response){
+                is Resource.Success ->{
+                    hideProgressBar()
+                    response.data?.let { newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles)
+                    }
+                }
+                is Resource.Error ->{
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Log.e(TAG, "An error occurred: $message")
+                    }
+                }
+                is Resource.Loading ->{
+                    showProgressBar()
+
+                }
+            }
+        })
+    }
+
+    private fun setupRecyclerView(){
+        newsAdapter = NewsAdapter()
+        rvBreakingNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+
+    private fun hideProgressBar(){
+        paginationProgressBar.visibility = View.INVISIBLE
+    }
+    private fun showProgressBar(){
+        paginationProgressBar.visibility = View.VISIBLE
+    }
 }
